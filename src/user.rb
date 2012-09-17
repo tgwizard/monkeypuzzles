da328ff8bccbce211db8ef3d377832ea25649581
adam <tgwizard@gmail.com>
2012-09-17 16:01:40 +0200

    Add gravatar image

--- src/user.rb
+++ src/user.rb
@@ -1,7 +1,10 @@
+require 'digest/md5'
+
 class User
-	attr_reader :email
+	attr_reader :email, :gravatar_url
 	def initialize(data)
 		@email = data[:email]
+		@gravatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(@email.strip.downcase)}"
 	end
 
 	def self.get(data)
