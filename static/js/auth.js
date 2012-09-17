4e525da96a22876e23c6b4020e24f392a8dbc4c0
adam <tgwizard@gmail.com>
2012-09-17 16:01:23 +0200

    Log login initiation/success and so

--- static/js/auth.js
+++ static/js/auth.js
@@ -2,19 +2,22 @@
 	navigator.id.watch({
 		loggedInEmail: currentUser,
 		onlogin: function(assertion) {
+			// TODO: perhaps show progress screen or something while verifying login
+			console.log("login initiated");
 			$.ajax({
 				type: 'POST',
 				url: '/auth/login',
 				data: {assertion: assertion},
-				success: function(res, status, xhr) { window.location.reload(); },
+				success: function(res, status, xhr) { console.log("login succeeded, reloading"); window.location.reload(); },
 				error: function(res, status, xhr) { console.log("failure"); console.log(res); alert("login failure " + res); }
 			});
 		},
 		onlogout: function() {
+			console.log("logout initiated");
 			$.ajax({
 				type: 'POST',
 				url: '/auth/logout',
-				success: function(res, status, xhr) { window.location.reload(); },
+				success: function(res, status, xhr) { console.log("logout succeeded, reloading"); window.location.reload(); },
 				error: function(res, status, xhr) { console.log("failure"); console.log(res); alert("logout failure " + res); }
 			});
 		}
