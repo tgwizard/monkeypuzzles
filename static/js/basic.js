298a2c135086cf98791cd7c03c65a81829785dfc
adam <tgwizard@gmail.com>
2012-09-18 16:06:36 +0200

    Added dummy console.log if console.log is undefined

--- static/js/basic.js
+++ static/js/basic.js
@@ -1,3 +1,11 @@
+if(typeof(console) === 'undefined') {
+	// so that we can have console.log statements in production
+	var console = {};
+	console.log = console.error = console.info = console.debug = console.warn =
+		console.trace = console.dir = console.dirxml = console.group =
+		console.groupEnd = console.time = console.timeEnd = console.assert =
+		console.profile = function() {};
+}
 (function($) {
 	$(document).ajaxError(function(e, xhr, settings, error) {
 		console.log("ajax error!");
