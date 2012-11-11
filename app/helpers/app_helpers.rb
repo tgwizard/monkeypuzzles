1f41ca20a77765d5bbb7d478e8d81569f3309022
adam <tgwizard@gmail.com>
2012-11-11 22:39:22 +0100

    Forgot the ga script

--- app/helpers/app_helpers.rb
+++ app/helpers/app_helpers.rb
@@ -6,4 +6,11 @@ MonkeyPuzzles.helpers do
   def csrf_tag
     Rack::Csrf.csrf_tag(env)
   end
+
+  # tracking script
+  def tracking_script
+    if settings.environment == :production
+      render "google_analytics", :layout => false
+    end
+  end
 end
